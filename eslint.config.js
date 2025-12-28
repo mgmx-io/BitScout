667b9e717bef0c6555bb7cd3f177379e39cd5e81
Martin Maldonado <tin.maldonado@gmail.com>
2025-12-28 16:59:01 -0300

    add navigation types

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 const pluginQuery = require("@tanstack/eslint-plugin-query");
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   ...pluginQuery.configs["flat/recommended"],
   {
     ignores: ["dist/*"],
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
   },
 ]);
