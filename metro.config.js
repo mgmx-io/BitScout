1b537a381e52cb1fee06550b14b28f2ea6a6a08a
Martin Maldonado <tin.maldonado@gmail.com>
2026-02-28 00:13:11 -0300

    run linter

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro'); 
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 module.exports = withUniwindConfig(config, {
-  cssEntryFile: './src/global.css',
+  cssEntryFile: "./src/global.css",
   dtsFile: "./src/types/uniwind-types.d.ts",
 });
